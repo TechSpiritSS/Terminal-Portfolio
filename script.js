3a4c8d7636ba4d1e44e871e3dfcf751ae63035a8
Saran-pariyar <spariyar05@gmail.com>
2022-10-04 08:11:40 +0545

    Removed if-else and used switched case for more code readability Gave an arrow for command show that It will be easier to see the commands

--- script.js
+++ script.js
@@ -59,74 +59,102 @@ async function getInputValue() {
     .querySelector("input")
     .value.replace(/\s+/g, "")
     .toLowerCase();
-  if (value === "help" || value === "ls") {
-    trueValue(value);
-    createCode("help", "for a list of commands");
-    createCode("clear", "to clear the terminal");
-    createCode("about", "to learn more about me");
-    createCode("social", "to see my social links");
-    createCode("projects", "to see my projects");
-    createText(
-      `<div onClick="exit()">EXIT</div>`
-    );
-  } else if (value === "about") {
-    trueValue(value);
-    createText(
-      "I am a Web Developer with a good knowledge of Data Structures and Algorithms along with SQL. I am currently Google DSC Lead, CodeChef Chapter Event Lead, and Co-Founder of Algoders Community at my Campus. I have 3-star rating at CodeChef. In 2022 I am learning MERN Stack and I am planning to work as a Full-Stack Developer."
-    );
-  } else if (value === "social") {
-    trueValue(value);
-    createText(
-      `<a href="https://github.com/techspiritss" target="_blank">GitHub</a>`
-    );
-    createText(
-      `<a href="https://www.linkedin.com/in/sidharthsethiss" target="_blank">LinkedIn</a>`
-    );
-    createText(
-      `<a href="https://leetcode.com/techspiritss" target="_blank">LeetCode</a>`
-    );
-    createText(
-      `<a href="https://www.codechef.com/users/techspiritss" target="_blank">CodeChef</a>`
-    );
-  } else if (value === "projects") {
-    trueValue(value);
-    createText("Projects:");
-    createText(
-      `<a href="https://techspiritss.github.io/MyChabi/" target="_blank">MyChabi</a> - MyChabi is a web application to help you out with your passwords`
-    );
-    createText(
-      `<a href="https://github.com/TechSpiritSS/Task-C-" target="_blank">Task C++</a> - A command-line based task management application`
-    );
-    createText(
-      `<a href="https://mohityadav0903.github.io/Notes-Insight" target="_blank">Notes Insight</a> - A note taking app for visually weak and elderly who aren't comfortable with Modern UI`
-    );
-    createText(
-      `<a href="https://github.com/TechSpiritSS/bigInt.git" target="_blank">BigINT Library</a> - This is my own C Library for BigINT made from scratch and it supports 2700 digits`
-    );
-    createText(
-      `<a href="https://techspiritss.github.io/50-Days-50-Projects-Web-Dev/" target="_blank">
-      50 Days of Web</a> - 50 Web Apps made over the period of 50 days for learning purpose`
-    );
-    createText(
-      `<a href="https://github.com/TechSpiritSS/Covid-Vaccine" target="_blank">
-      Covid Vaccination Slot</a> - This Python Program informs about the available Covid vaccine slots at your pin code according to your age group.`
-    );
-  } else if (value === "clear" || value === "cls") {
-    document.querySelectorAll("p").forEach((e) => e.parentNode.removeChild(e));
-    document
-      .querySelectorAll("section")
-      .forEach((e) => e.parentNode.removeChild(e));
-  } else if (value === "sudo") {
-    trueValue(value);
-    createText("You are not authorized to use this command");
-  } else if (value === "cd") {
-    trueValue(value);
-    createText("There's no directory in this path");
-  } 
-  else {
-    falseValue(value);
-    createText(`${value} is not a valid command`);
+
+  switch (value) {
+    case "help":
+    case "ls":
+      trueValue(value);
+      createCode("help", "for a list of commands");
+      createCode("clear", "to clear the terminal");
+      createCode("about", "to learn more about me");
+      createCode("social", "to see my social links");
+      createCode("projects", "to see my projects");
+      createText(
+        `<div onClick="exit()">EXIT</div>`
+      );
+      break;
+
+
+    case "about":
+
+      trueValue(value);
+      createText(
+        "I am a Web Developer with a good knowledge of Data Structures and Algorithms along with SQL. I am currently Google DSC Lead, CodeChef Chapter Event Lead, and Co-Founder of Algoders Community at my Campus. I have 3-star rating at CodeChef. In 2022 I am learning MERN Stack and I am planning to work as a Full-Stack Developer."
+      );
+      break;
+
+    case "social":
+
+      trueValue(value);
+      createText(
+        `<a href="https://github.com/techspiritss" target="_blank">GitHub</a>`
+      );
+      createText(
+        `<a href="https://www.linkedin.com/in/sidharthsethiss" target="_blank">LinkedIn</a>`
+      );
+      createText(
+        `<a href="https://leetcode.com/techspiritss" target="_blank">LeetCode</a>`
+      );
+      createText(
+        `<a href="https://www.codechef.com/users/techspiritss" target="_blank">CodeChef</a>`
+      );
+      break;
+
+
+    case "projects":
+
+      trueValue(value);
+      createText("Projects:");
+      createText(
+        `<a href="https://techspiritss.github.io/MyChabi/" target="_blank">MyChabi</a> - MyChabi is a web application to help you out with your passwords`
+      );
+      createText(
+        `<a href="https://github.com/TechSpiritSS/Task-C-" target="_blank">Task C++</a> - A command-line based task management application`
+      );
+      createText(
+        `<a href="https://mohityadav0903.github.io/Notes-Insight" target="_blank">Notes Insight</a> - A note taking app for visually weak and elderly who aren't comfortable with Modern UI`
+      );
+      createText(
+        `<a href="https://github.com/TechSpiritSS/bigInt.git" target="_blank">BigINT Library</a> - This is my own C Library for BigINT made from scratch and it supports 2700 digits`
+      );
+      createText(
+        `<a href="https://techspiritss.github.io/50-Days-50-Projects-Web-Dev/" target="_blank">
+            50 Days of Web</a> - 50 Web Apps made over the period of 50 days for learning purpose`
+      );
+      createText(
+        `<a href="https://github.com/TechSpiritSS/Covid-Vaccine" target="_blank">
+            Covid Vaccination Slot</a> - This Python Program informs about the available Covid vaccine slots at your pin code according to your age group.`
+      );
+      break;
+
+    case "clear":
+    case "cls":
+
+      document.querySelectorAll("p").forEach((e) => e.parentNode.removeChild(e));
+      document
+        .querySelectorAll("section")
+        .forEach((e) => e.parentNode.removeChild(e));
+      break;
+
+
+    case "sudo":
+
+      trueValue(value);
+      createText("You are not authorized to use this command");
+      break;
+
+    case "cd":
+      trueValue(value);
+      createText("There's no directory in this path");
+      break;
+
+    default:
+      falseValue(value);
+      createText(`${value} is not a valid command`);
   }
+
+
+
 }
 
 function trueValue(value) {
@@ -166,7 +194,7 @@ function createText(text) {
 
 function createCode(code, text) {
   const p = document.createElement("p");
-  p.innerHTML = `<span class="code">${code}</span> ${text}`;
+  p.innerHTML = `<span class="code">${code} =></span> ${text}`;
   app.appendChild(p);
 }
 
