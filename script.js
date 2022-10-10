6c9744f5fae549a822260a7d34d9ecd743c62e4c
YashM246 <94853015+YashM246@users.noreply.github.com>
2022-10-10 21:37:20 +0530

    Dark Theme Toggle Button

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-import config from "./config.json" assert { type: "json" };
-
 const app = document.querySelector("#app");
 const bodyContainer = document.querySelector("#bodyContainer");
-const greenButton = document.querySelector("#greenButton");
-const yellowButton = document.querySelector("#yellowButton");
-const redButton = document.querySelector("#redButton");
+const greenButton = document.querySelector("#greenButton")
+const yellowButton = document.querySelector("#yellowButton")
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const history = [];
-const contributors = [];
-const userBlogs = [];
-const IpDetails = [];
-const userRepos = [];
 var count = 0;
-const commandsList = [
-  "help",
-  "clear",
-  "about",
-  "social",
-  "projects",
-  "repos",
-  "cheer",
-  "ipconfig",
-  "contributors",
-  "neofetch",
-];
 
 greenButton.addEventListener("click", () => {
   const container = document.querySelector("#screenContainer");
   container.classList.contains("maximized")
-    ? container.classList.remove("maximized")
-    : container.classList.add("maximized");
+  ? container.classList.remove("maximized")
+  : container.classList.add("maximized");
+
+
+  if(bodyContainer.classList.contains("minimized"))
+    bodyContainer.classList.remove("minimized")
 });
 
 yellowButton.addEventListener("click", () => {
   bodyContainer.classList.contains("minimized")
-    ? bodyContainer.classList.remove("minimized")
-    : bodyContainer.classList.add("minimized");
-});
-
-redButton.addEventListener("click", () => {
-  exit();
-});
+  ? bodyContainer.classList.remove("minimized")
+  : bodyContainer.classList.add("minimized")
+})
 
 app.addEventListener("keydown", async function (event) {
   if (event.key === "Enter") {
@@ -49,48 +30,21 @@ app.addEventListener("keydown", async function (event) {
     removeInput();
     await delay(150);
     new_line();
-    count = history.length;
   }
   if (event.key === "ArrowUp") {
-    if (count > 0) {
+    if (count > 0){
       const input = document.querySelector("input");
       input.value = history[--count];
     }
   }
   if (event.key === "ArrowDown") {
-    if (count < history.length - 1) {
+    if (count < history.length-1){
       const input = document.querySelector("input");
       input.value = history[++count];
-    } else {
+    }
+    else{
       const input = document.querySelector("input");
       input.value = "";
-    }
-  }
-  if (event.key === "Tab") {
-    event.preventDefault();
-    const input = document.querySelector("input");
-    const toComplete = input.value;
-    const completed = commandsList.find((command) =>
-      command.startsWith(toComplete)
-    );
-    if (toComplete && completed) {
-      // autocomplete if there was something typed in and it matches the start
-      // of some command
-      input.value = completed;
-    }
-  }
-  if (event.ctrlKey) {
-    if(event.key === "l" || event.key === "L") {
-      document
-        .querySelectorAll("p")
-        .forEach((e) => e.parentNode.removeChild(e));
-      document
-        .querySelectorAll("section")
-        .forEach((e) => e.parentNode.removeChild(e));
-      removeInput();
-      await delay(150);
-      new_line();
-      count = history.length;
     }
   }
 });
@@ -116,9 +70,9 @@ function new_line() {
   const span = document.createElement("span");
   const span2 = document.createElement("span");
   p.setAttribute("class", "path");
-  p.textContent = config.terminal.user + " ";
-  span.textContent = config.terminal.host + " ";
-  span2.textContent = config.terminal.path + " ";
+  p.textContent = "$Sidharth_Sethi";
+  span.textContent = " sudo";
+  span2.textContent = " ~/guest";
   p.appendChild(span);
   p.appendChild(span2);
   app.appendChild(p);
@@ -138,32 +92,14 @@ function removeInput() {
   app.removeChild(div);
 }
 
-async function fetchGithubStats() {
-  const githubLink = config.social.find((c) => c.title === "Github").link;
-  const githubUsername =
-    githubLink.split("/")[githubLink.split("/").length - 1];
-  const responseRaw = await fetch(
-    `https://api.github.com/users/${githubUsername}`
-  );
-  const response = await responseRaw.json();
-
-  createText(`Github Bio: ${response.bio}`);
-  createText(`Number of repositories : ${response.public_repos}`);
-  createText(`Number of gists: ${response.public_gists}`);
-  createText(`Number of followers: ${response.followers}`);
-  createText(`Number of following: ${response.following}`);
-}
-
 async function getInputValue() {
-  const value = document.querySelector("input").value.trim().toLowerCase();
-  if (value.substring(0, 5) === "cheer") {
-    value.substring(0, 5).toLowerCase();
-  } else {
-    value.replace(/\s+/g, "").toLowerCase();
-  }
+  const value = document
+    .querySelector("input")
+    .value.replace(/\s+/g, "")
+    .toLowerCase();
 
-  history.push(document.querySelector("input").value);
-  count++;
+    history.push(document.querySelector("input").value);
+    count++;
 
   switch (value) {
     case "help":
@@ -174,140 +110,92 @@ async function getInputValue() {
       createCode("about", "to learn more about me");
       createCode("social", "to see my social links");
       createCode("projects", "to see my projects");
-      createCode("blogs", "to see my recent blogs");
-      createCode("contact", "to enquire about my services");
-      createCode("cheer", "to appreciate my work");
-      createCode("repos", "to see my github repositories");
-      createCode("ipconfig", "to see your IP details");
-      createCode("github", "to see my github stats");
-      createCode("contributors", "to see all the contributors");
-      break;
-    case "neofetch":
-      neofetch();
+      createText(
+        `<div onClick="exit()">EXIT</div>`
+      );
       break;
 
+
     case "about":
+
       trueValue(value);
-      createText(config.about);
+      createText(
+        "I am a Web Developer with a good knowledge of Data Structures and Algorithms along with SQL. I am currently Google DSC Lead, CodeChef Chapter Event Lead, and Co-Founder of Algoders Community at my Campus. I have 3-star rating at CodeChef. In 2022 I am learning MERN Stack and I am planning to work as a Full-Stack Developer."
+      );
       break;
 
     case "social":
+
       trueValue(value);
-      config.social.forEach((item) => {
-        createText(`<a href=${item.link} target="_blank">${item.title}</a>`);
-      });
+      createText(
+        `<a href="https://github.com/techspiritss" target="_blank">GitHub</a>`
+      );
+      createText(
+        `<a href="https://www.linkedin.com/in/sidharthsethiss" target="_blank">LinkedIn</a>`
+      );
+      createText(
+        `<a href="https://leetcode.com/techspiritss" target="_blank">LeetCode</a>`
+      );
+      createText(
+        `<a href="https://www.codechef.com/users/techspiritss" target="_blank">CodeChef</a>`
+      );
       break;
+
 
     case "projects":
+
       trueValue(value);
       createText("Projects:");
-      config.projects.forEach((item) => {
-        createText(
-          `<a href=${item.link} target="_blank">${item.title}</a> - ${item.description}`
-        );
-      });
-      break;
-
-    case "blogs":
-      trueValue(value);
-      createText("Recent Blogs:");
-      // Hashnode Feed URL: https://username.hashnode.dev/rss.xml
-      // Dev.to Feed URL: https://dev.to/feed/username
-      // Medium Feed URL: https://medium.com/feed/@username
-      // TODO: Insert your Medium/Dev/Hashnode or any blog feed URL below
-      userBlogs.forEach((blog) => {
-        createText(`${blog.site}: `);
-        blog.items.forEach((item, index) => {
-          createText(
-            `<a href="${item.link}" target="_blank">${index + 1}. ${
-              item.title
-            }</a>`
-          );
-        });
-      });
-      break;
-
-    case "contributors":
-      trueValue(value);
-      contributors.forEach((user) => {
-        createText(`- <a href=${user.userProfile}>${user.username}</a>`);
-      });
-      createText(`- Thanks to all the contributors 💖`);
-      break;
-
-    case "ipconfig":
-      trueValue(value);
-      const IP = IpDetails[0];
-      createText(`- Ipv6: ${IP.ip}`);
-      createText(`- network: ${IP.network}`);
-      createText(`- city: ${IP.city}`);
-      createText(`- network org: ${IP.org}`);
-      createText(`- region: ${IP.region}`);
-      createText(`- postal: ${IP.postal}`);
-      break;
-
-    case "repos":
-      trueValue(value);
-      userRepos[0].forEach((repo, index) => {
-        createText(
-          `- repo_${index} name: <a href=${repo.html_url}>${
-            repo.name
-          }</a> | language: ${
-            repo.language === null ? "no language" : repo.language
-          }`
-        );
-        createText(
-          `_ description: ${
-            repo.description === null ? "no description." : repo.description
-          }`
-        );
-      });
+      createText(
+        `<a href="https://techspiritss.github.io/MyChabi/" target="_blank">MyChabi</a> - MyChabi is a web application to help you out with your passwords`
+      );
+      createText(
+        `<a href="https://github.com/TechSpiritSS/Task-C-" target="_blank">Task C++</a> - A command-line based task management application`
+      );
+      createText(
+        `<a href="https://mohityadav0903.github.io/Notes-Insight" target="_blank">Notes Insight</a> - A note taking app for visually weak and elderly who aren't comfortable with Modern UI`
+      );
+      createText(
+        `<a href="https://github.com/TechSpiritSS/bigInt.git" target="_blank">BigINT Library</a> - This is my own C Library for BigINT made from scratch and it supports 2700 digits`
+      );
+      createText(
+        `<a href="https://techspiritss.github.io/50-Days-50-Projects-Web-Dev/" target="_blank">
+            50 Days of Web</a> - 50 Web Apps made over the period of 50 days for learning purpose`
+      );
+      createText(
+        `<a href="https://github.com/TechSpiritSS/Covid-Vaccine" target="_blank">
+            Covid Vaccination Slot</a> - This Python Program informs about the available Covid vaccine slots at your pin code according to your age group.`
+      );
       break;
 
     case "clear":
     case "cls":
-      document
-        .querySelectorAll("p")
-        .forEach((e) => e.parentNode.removeChild(e));
+
+      document.querySelectorAll("p").forEach((e) => e.parentNode.removeChild(e));
       document
         .querySelectorAll("section")
         .forEach((e) => e.parentNode.removeChild(e));
-      removeNeoFetch();
       break;
-    case "contact":
-      createText(
-        `Hey! Would love to get in touch. Drop me a text at <a href="mailto:${config.contact.email}" target="_blank">${config.contact.email}</a>`
-      );
-      window.location.href = `mailto:${config.contact.email}`;
-      break;
+
+
     case "sudo":
+
       trueValue(value);
       createText("You are not authorized to use this command");
-      break;
-    case "github":
-      trueValue(value);
-      fetchGithubStats();
       break;
 
     case "cd":
       trueValue(value);
       createText("There's no directory in this path");
       break;
-    case "exit":
-      window.close();
+
     default:
-      if (value.substring(0, 5) === "cheer") {
-        trueValue(value);
-        const reply =
-          config.cheer.responseArray[
-            Math.floor(Math.random() * config.cheer.responseArray.length)
-          ];
-        createText(reply);
-      } else {
-        falseValue(value);
-        createText(`${value} is not a valid command`);
-      }
+      falseValue(value);
+      createText(`${value} is not a valid command`);
   }
+
+
+
 }
 
 function trueValue(value) {
@@ -351,171 +239,38 @@ function createCode(code, text) {
   app.appendChild(p);
 }
 
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+const container = document.querySelector('container');
+const menu = document.querySelector('.menu');
+const bb = document.querySelector('.bottom-bar');
+
+toggle.addEventListener('click', function(){
+  this.classList.toggle('bi-moon');
+
+  if(this.classList.toggle('bi-brightness-high-fill')){
+    body.style.background = 'black';
+    body.style.color = 'white';
+    body.style.transition = '2s';
+    menu.style.backgroundColor = '#B958A5';
+    menu.style.transition = '2s';
+    app.style.backgroundColor = '#9145B6';
+    app.style.transition = '2s';
+    bb.style.backgroundColor = '#4C3F91';
+    bb.style.transition = '2s';
+
+  }
+  else{
+    body.style.background = 'white';
+    body.style.color = 'black';
+    body.style.transition = '2s';
+    menu.style.backgroundColor = '#faa094';
+    menu.style.transition = '2s';
+    app.style.backgroundColor = '#ffdde2';
+    app.style.transition = '2s';
+    bb.style.backgroundColor = '#fc766a';
+    bb.style.transition = '2s';
+  }
+})
+
 openTerminal();
-
-//creat neofetch
-
-function neofetch() {
-  const data = {
-    name: "Shethi Sidharth",
-    title: "developer",
-    skills: "Frontend, Backend",
-    shell: "zsh",
-    langauges: "Javascript, Python, CSS, DB",
-  };
-  const container = document.createElement("div");
-  container.classList.add("fetch-container");
-
-  const fimg = document.createElement("div");
-  fimg.classList.add("fetch-img-container");
-  fimg.innerHTML = "<img class='fetch-img' src='js.png' />";
-
-  const info = document.createElement("div");
-  info.classList.add("info");
-  container.appendChild(fimg);
-  container.appendChild(info);
-
-  for (const [key, value] of Object.entries(data)) {
-    const p = document.createElement("p");
-    p.innerHTML = `<span class="key">${key}</span>: <span class="value">${value}</span>`;
-    info.appendChild(p);
-  }
-
-  app.appendChild(container);
-}
-
-function removeNeoFetch() {
-  document.querySelector(".fetch-container").remove();
-}
-// get the contributors list
-
-const getContributors = async () => {
-  try {
-    const response = await fetch(
-      "https://api.github.com/repos/TechSpiritSS/Terminal-Portfolio/contributors"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        data.forEach((user) => {
-          const userDetails = { username: "", userProfile: "" };
-          userDetails.username = user.login;
-          userDetails.userProfile = user.html_url;
-          contributors.push(userDetails);
-        });
-      });
-  } catch (error) {
-    console.log(error);
-    // handling the error
-    contributors.push({
-      username: "__network_error __check internet connection",
-      userProfile: " ",
-    });
-  }
-};
-
-getContributors();
-
-const getBlogs = async () => {
-  config.blogs.forEach(async (blog) => {
-    try{
-      const rssConverter = `https://api.rss2json.com/v1/api.json?rss_url=${blog.url}`;
-      const response = await fetch(rssConverter);
-      const data = await response.json();
-      userBlogs.push({site:blog.site, items:data.items});
-    } catch (error) {
-      console.log(error);
-      // handling the error
-      userBlogs.push({
-        site: blog.site,
-        items: ["_failed_to_fetch_"],
-      });
-    }
-  });
-};
-
-getBlogs();
-
-// ip lookup --> https://ipapi.co/json
-
-const getIPDetails = async () => {
-  try {
-    const response = await fetch("https://ipapi.co/json")
-      .then((response) => response.json())
-      .then((data) => {
-        IpDetails.push(data);
-      });
-  } catch (error) {
-    console.log(error);
-    // handling the error
-    IpDetails.push({
-      ip: "__network_error",
-      network: "__kindly check internet connection",
-      city: "",
-      region: "",
-      org: "",
-      postal: "",
-    });
-  }
-};
-
-getIPDetails();
-
-// get user github repositories
-
-const getRepo = async () => {
-  try {
-    const response = await fetch(
-      "https://api.github.com/users/TechSpiritSS/repos"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        userRepos.push(data);
-      });
-  } catch (error) {
-    console.log(error);
-    userRepos.push([
-      {
-        name: "__network_error",
-        description: "__kindly check internet connection",
-        html_url: "",
-      },
-    ]);
-  }
-};
-
-getRepo();
-
-// Themes Switcher
-
-let switches = document.getElementsByClassName("switch");
-
-let style = localStorage.getItem("style");
-
-if (style == null) {
-  setTheme("default");
-} else {
-  setTheme(style);
-}
-
-for (let i of switches) {
-  i.addEventListener("click", function () {
-    let theme = this.dataset.theme;
-    setTheme(theme);
-  });
-}
-
-function setTheme(theme) {
-  if (theme == "nature") {
-    document.getElementById("switcher-id").href = "./themes/nature.css";
-  } else if (theme == "sky") {
-    document.getElementById("switcher-id").href = "./themes/sky.css";
-  } else if (theme == "matrix") {
-    document.getElementById("switcher-id").href = "./themes/matrix.css";
-  } else if (theme == "metalic") {
-    document.getElementById("switcher-id").href = "./themes/metalic.css";
-  } else if (theme == "default") {
-    document.getElementById("switcher-id").href = "./themes/default.css";
-  }
-
-  localStorage.setItem("style", theme);
-}
